9601aa34ac08d029fa92a3236a2a8c6db8884e13
Tiara <tiarinix@gmail.com>
2024-11-17 19:50:46 -0300

    Todo terminado y ordenado 	modified:   Lab3.ipynb 	deleted:    Lab4.ipynb 	deleted:    a.py 	deleted:    ee_test.py 	deleted:    modelo6 	deleted:    requirements.txt

--- ee_test.py
+++
@@ -1,8 +0,0 @@
-import ee
-
-# Inicializar la conexión con Earth Engine
-try:
-    ee.Initialize()
-    print("Earth Engine Initialized Successfully!")
-except Exception as e:
-    print(f"Error initializing Earth Engine: {e}")
